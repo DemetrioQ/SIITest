4918c98899959d7929a920befaf40a43368db54d
DemetrioQ <demetriorqe@gmail.com>
2022-09-15 00:51:08 -0400

    Now changes inputs while user is inputing them

--- src/components/CardForm/CardContainer/CardContainer.jsx
+++ src/components/CardForm/CardContainer/CardContainer.jsx
@@ -1,25 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import visaImage from '../../../assets/visa.png';
 import chipImage from '../../../assets/chip.png';
 import cardImage from '../../../assets/cardFront.png';
 
-const CardContainer = () => {
-    let cardNumber = '5375441145400954'.match(/.{1,4}/g);
-    let cardNumberFormnatted = cardNumber.join(' ').toString();
+const CardContainer = ({ cardNumber, name, expDate }) => {
+    const [cardNumberFormatted, setCardNumberFormatted] = useState('5375 4411 4540 0954');
+    useEffect(() => {
+        if (!cardNumber) {
+            setCardNumberFormatted('');
+            return;
+        }
+        
+        const formatted = cardNumber
+            ?.match(/.{1,4}/g)
+            ?.join(' ')
+            .toString();
+
+        setCardNumberFormatted(formatted);
+
+        return;
+    }, [cardNumber]);
+
+
     return (
         <div className='card-container'>
             <div className='front'>
                 <div className='image'>
-                    <img src={cardImage} alt='Card' className='card-image' />
+                    <img src={cardImage} alt='Card' className='card-image' draggable='false' />
                 </div>
-                <div className='card-number-box'>s</div>
+                <div className='card-number-box card-container-text'>
+                    <span className='card-container-text'>{cardNumberFormatted}</span>
+                </div>
                 <div className='flexbox'>
                     <div className='box'>
-                        <div className='card-holder-name'>full name</div>
+                        <div className='card-holder-name'>
+                            <span className='card-container-text'>{name.toUpperCase()}</span>
+                        </div>
                     </div>
                     <div className='box'>
                         <div className='expiration'>
-                            <span className='exp-month'>mm</span>
+                            <span className='exp-month card-container-text'>{expDate}</span>
                         </div>
                     </div>
                 </div>
